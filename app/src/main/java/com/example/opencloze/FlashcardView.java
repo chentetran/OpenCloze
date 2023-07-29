005aa1cf35b681db48d650c5ff7137fe00c01606
chentetran <vincenttran23@gmail.com>
2023-07-28 22:43:53 -0400

    UPDATE

---
+++ app/src/main/java/com/example/opencloze/FlashcardView.java
@@ -0,0 +1,25 @@
+package com.example.opencloze;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class FlashcardView extends View {
+    public FlashcardView(Context context) {
+        super(context);
+    }
+
+    public FlashcardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlashcardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public FlashcardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
